ff01e5ba43e7b43d5f07b08a1048c85f53ec7e83
Siva <sivakaruturi9@gmail.com>
2025-05-14 11:01:12 +0530

    Initial Commit

---
+++ We Split/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  We Split
+//
+//  Created by Karuturi Siva Rama Krishna on 14/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ We Split/We_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  We_SplitApp.swift
+//  We Split
+//
+//  Created by Karuturi Siva Rama Krishna on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct We_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
